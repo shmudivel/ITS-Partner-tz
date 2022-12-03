0f67673c85e668a4e7218ed8f0e659b3dbf41cca
shmudivel <82043832+shmudivel@users.noreply.github.com>
2022-12-03 11:38:37 -0500

    caroulsel / slideshow html css base

--- javascript.js
+++ javascript.js
@@ -1,7 +1,30 @@
+// Mobile version hamburger bars
 const menu = document.querySelector("#mobile-menu");
-const menuLingks = document.querySelector(".navbar__menu");
+const menuLinks = document.querySelector(".navbar__menu");
 
 menu.addEventListener("click", function () {
   menu.classList.toggle("is-active");
-  menuLingks.classList.toggle("active");
+  menuLinks.classList.toggle("active");
 });
+
+// Carousel
+const track = document.querySelector(".carousel__track");
+const slides = Array.from(track.children);
+const nextButton = document.querySelector(".carousel__button--right");
+const prevButton = document.querySelector(".carousel__button--left");
+const dotsNav = document.querySelector(".carousel__nav");
+const dots = Array.from(dotsNav.children);
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+// Slides next to one another
+const setSlidePosition = (slide, index) => {
+  slide.style.left = slideWidth * index + "px";
+};
+slides.forEach(setSlidePosition);
+
+// Click left
+
+// Click right
+
+// Dot indicator moves (lights up) by click to current slide
